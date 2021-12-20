ddbb5e4c2d0e76158c37dbe2e1c1e613955141fc
mauricio <mauricio945549@gmail.com>
2021-12-20 13:26:48 -0500

    primer commit

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+let correct_button = document.getElementById("correct");
+let incorrect_button1 = document.getElementsByClassName("incorrect1");
+
+correct_button.addEventListener("click",()=>{
+
+   window.open("https://slideplayer.es/slide/13727418/85/images/2/FELICIDADES+GANASTE%21%21+SALIR+DEL+JUEGO.jpg");
+
+});
+
+incorrect_button1.addEventListener("click",()=>{
+    window.open("https://slideplayer.es/slide/13727418/85/images/3/PERDISTE+VUELVE+INTENTARLO.jpg");
+
+});
